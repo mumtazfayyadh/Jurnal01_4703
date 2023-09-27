2b58c7240e4f4a6c806b18c78915b17afa274327
mumtazfayyadh <mumtazfayyadh@gmail.com>
2023-09-27 13:15:00 +0700

    initial commit

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02{
+    public static void main(String[] args){
+        	System.out.println("Nama  : Abdullah Mumtaz Fayyadh");
+			System.out.println("Nim   : 607062300102");
+			System.out.println("kelas : D3IF-47-03");
+    }
+}
